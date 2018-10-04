ab095207e55b53753f214d4f68a65b067116b143
Christopher Britt <christopher.brittjr@outlook.com>
2018-10-04 09:49:55 -0400

    Add project files.

---
+++ EssentialTools/Models/Discount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssentialTools.Models
+{
+    public interface IDiscountHelper
+    {
+        decimal ApplyDiscount(decimal totalParam);
+    }
+    public class DefaultDiscountHelper : IDiscountHelper
+    {
+        public decimal discountSize;
+        private decimal discountParam;
+
+        public DefaultDiscountHelper(decimal totalParam)
+        {
+            discountSize = discountParam;
+        }        
+
+        public decimal ApplyDiscount(decimal totalParam)
+        {
+            return (totalParam - (discountSize /100m * totalParam));
+        }
+    }
+}
